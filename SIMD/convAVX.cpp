c8e13474cc59c3beb7e989b71dac174a1e05c328
Viranchee Lotia <hey@viranchee.com>
2025-02-05 12:57:52 -0600

    refactor

--- SIMD/convAVX.cpp
+++
@@ -1,2 +0,0 @@
-#include "conv.h"
-#include <immintrin.h>
--- SIMD/kernels.h
+++ SIMD/kernels.h
@@ -1,7 +1,14 @@
 #include <vector>
+
 enum class Computation { Scalar, AVX, NEON };
 
 template <Computation C>
 std::vector<float> convolution_1d(std::vector<float> input,
                                   std::vector<float> kernel, int padding,
                                   int stride);
+
+template <Computation C, typename T>
+void convolution_1d(T *input, int inputSize, T *kernel, int kernelSize,
+                    T *output, int **outputSize, int padding, int stride);
+
+template <Computation C, typename T> T *vectorAdd(T *v1, T *v2, int size);
