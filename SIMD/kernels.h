a4102ff3852f206a0eaeb4cf1e291788c0ca6109
Viranchee Lotia <hey@viranchee.com>
2025-02-06 14:43:50 -0600

    add virtual

--- SIMD/kernels.h
+++ SIMD/kernels.h
@@ -19,19 +19,19 @@ void convolution_1d(T *input, int inputSize, T *kernel, int kernelSize,
 template <typename T> class SIMD {
 public:
   // Prefix Sum
-  T prefixSum(T *v, int size) = 0;
+  virtual T prefixSum(T *v, int size) = 0;
   // Vector Add
-  T *vectorAdd(T *v1, T *v2, int size) = 0;
+  virtual T *vectorAdd(T *v1, T *v2, int size) = 0;
   // Add all elements in the vector
-  T vectorReduce(T *v, int size) = 0;
+  virtual T vectorReduce(T *v, int size) = 0;
   // Vector Max
-  T vectorMax(T *v, int size) = 0;
+  virtual T vectorMax(T *v, int size) = 0;
   // Vector Min
-  T vectorMin(T *v, int size) = 0;
+  virtual T vectorMin(T *v, int size) = 0;
   // Convolution 1D
-  T *convolution_1d(T *input, int iSize, T *kernel, int kSize, int **oSize,
-                    int padding, int stride) = 0;
+  virtual T *convolution_1d(T *input, int iSize, T *kernel, int kSize,
+                            int **oSize, int padding, int stride) = 0;
   // MatMul gemm
-  T *matMul(T *A, int aRows, int aCols, T *B, int bRows, int bCols, T **C,
-            int **cRows, int **cCols) = 0;
+  virtual T *matMul(T *A, int aRows, int aCols, T *B, int bRows, int bCols,
+                    T **C, int **cRows, int **cCols) = 0;
 };
