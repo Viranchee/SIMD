fa9cc0f83aa836d866d22268c0c77c29c0baaa9a
Viranchee Lotia <hey@viranchee.com>
2025-02-06 23:25:13 -0600

    scalar conv2D

--- SIMD/scalars.cpp
+++ SIMD/scalars.cpp
@@ -1,6 +1,7 @@
 #ifndef SCALAR_H
 #define SCALAR_H
 #include "kernels.h"
+#include <cassert>
 #include <cstdint>
 
 class Scalar : public SIMD<int8_t> {
@@ -47,9 +48,7 @@ public:
   }
   virtual int8_t *convolution_1d(int8_t *input, int iSize, int8_t *kernel,
                                  int kSize, int **oSize, int padding,
-                                 int stride) override {
-    return nullptr;
-  }
+                                 int stride) override {}
   virtual int8_t *matMul(int8_t *A, int M, int8_t *B, int N, int K) override {
     int8_t *result = new int8_t[M * N];
     for (int i = 0; i < M; i++) {
@@ -62,6 +61,34 @@ public:
       }
     }
     return result;
+  }
+  virtual int8_t *convolution_2d(int8_t *input, int iRows, int iCols,
+                                 int8_t *kernel, int kRows, int kCols,
+                                 int **oRows, int **oCols, int padding,
+                                 int stride) override {
+    int outRows = (iRows + 2 * padding - kRows) / stride + 1;
+    int outCols = (iCols + 2 * padding - kCols) / stride + 1;
+    *oRows = new int(outRows);
+    *oCols = new int(outCols);
+    int8_t *output = new int8_t[outRows * outCols];
+    for (int i = 0; i < outRows; i++) {
+      for (int j = 0; j < outCols; j++) {
+        int sum = 0;
+        for (int k = 0; k < kRows; k++) {
+          for (int l = 0; l < kCols; l++) {
+            int inputRow = i * stride + k;
+            int inputCol = j * stride + l;
+            if (inputRow < 0 || inputRow >= iRows || inputCol < 0 ||
+                inputCol >= iCols) {
+              continue;
+            }
+            sum += input[inputRow * iCols + inputCol] * kernel[k * kCols + l];
+          }
+        }
+        output[i * outCols + j] = sum;
+      }
+    }
+    return output;
   }
 };
 
